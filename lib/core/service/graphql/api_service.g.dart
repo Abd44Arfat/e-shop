54396ae85741dd23d635d999b6fd5269ddb93cb6
Abdelrahman arfat <abdoarfat924@gmail.com>
2024-04-01 16:32:12 +0200

    finish dashboard model

--- lib/core/service/graphql/api_service.g.dart
+++ lib/core/service/graphql/api_service.g.dart
@@ -22,7 +22,7 @@ class _ApiService implements ApiService {
 
   @override
   Future<LoginResponse> login(Map<String, dynamic> mutation) async {
-    const _extra = <String, dynamic>{};
+    final _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
     final _headers = <String, dynamic>{};
     final _data = <String, dynamic>{};
@@ -50,10 +50,10 @@ class _ApiService implements ApiService {
 
   @override
   Future<UserRoleResponse> userRole() async {
-    const _extra = <String, dynamic>{};
+    final _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
     final _headers = <String, dynamic>{};
-    final Map<String, dynamic>? _data = null;
+    const Map<String, dynamic>? _data = null;
     final _result = await _dio
         .fetch<Map<String, dynamic>>(_setStreamType<UserRoleResponse>(Options(
       method: 'GET',
@@ -77,7 +77,7 @@ class _ApiService implements ApiService {
 
   @override
   Future<UploadImageResourse> uploadImage(FormData file) async {
-    const _extra = <String, dynamic>{};
+    final _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
     final _headers = <String, dynamic>{};
     final _data = file;
@@ -104,7 +104,7 @@ class _ApiService implements ApiService {
 
   @override
   Future<SignUpResponse> signUp(Map<String, dynamic> mutation) async {
-    const _extra = <String, dynamic>{};
+    final _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
     final _headers = <String, dynamic>{};
     final _data = <String, dynamic>{};
